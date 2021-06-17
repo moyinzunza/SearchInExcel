f03c3235ec01a4e1ecf80f5638b52df50665b56f
Moy <moyinzunza@gmail.com>
2021-06-17 11:36:35 -0500

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace SearchInExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.logBox = new System.Windows.Forms.TextBox();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(663, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(644, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // logBox
+            // 
+            this.logBox.Location = new System.Drawing.Point(13, 79);
+            this.logBox.Multiline = true;
+            this.logBox.Name = "logBox";
+            this.logBox.ReadOnly = true;
+            this.logBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logBox.Size = new System.Drawing.Size(775, 359);
+            this.logBox.TabIndex = 2;
+            // 
+            // searchBox
+            // 
+            this.searchBox.Location = new System.Drawing.Point(13, 50);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(644, 23);
+            this.searchBox.TabIndex = 3;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(663, 50);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(125, 23);
+            this.searchButton.TabIndex = 4;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox logBox;
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.Button searchButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace SearchInExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog diag = new FolderBrowserDialog();
+            if (diag.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string folder = diag.SelectedPath;  //selected folder path
+
+                textBox1.Text = folder;
+
+            }
+        }
+        public WorkbookPart ImportExcel(string filepath)
+        {
+            try
+            {
+
+                using (FileStream fs = File.Open(filepath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    MemoryStream m_ms = new MemoryStream();
+                    fs.CopyTo(m_ms);
+
+                    SpreadsheetDocument m_Doc = SpreadsheetDocument.Open(m_ms, false);
+
+                    return m_Doc.WorkbookPart;
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Trace.TraceError(ex.Message + ex.StackTrace);
+            }
+            return null;
+        }
+
+        public void GetIndexBySearch(string search, string filepath)
+        {
+            logBox.Text = logBox.Text + Environment.NewLine + filepath;
+            string[] letters = { "A", 
+                "B", 
+                "C", 
+                "D",
+                "E",
+                "F",
+                "G",
+                "H",
+                "I",
+                "J",
+                "K",
+                "L",
+                "M",
+                "N",
+                "O",
+                "P",
+                "Q",
+                "R",
+                "S",
+                "T",
+                "U",
+                "V",
+                "W",
+                "X",
+                "Y",
+                "Z"};
+            WorkbookPart workbookPart = ImportExcel(filepath);
+            var sheets = workbookPart.Workbook.Descendants<Sheet>();
+
+            foreach(var sheet in sheets) {
+
+                string index = "Not Found";
+
+                if (sheet != null)
+                {
+
+                    logBox.Text = logBox.Text + Environment.NewLine + sheet.Name;
+                    var worksheetPart = (WorksheetPart)workbookPart.GetPartById(sheet.Id);
+                    var rows = worksheetPart.Worksheet.Descendants<Row>().ToList();
+
+                    // Remove the header row
+                    rows.RemoveAt(0);
+
+                    foreach (var row in rows)
+                    {
+                        var cellss = row.Elements<Cell>().ToList();
+
+                        foreach (var cell in cellss)
+                        {
+                            var value = cell.InnerText;
+
+                            //logBox.Text = logBox.Text + Environment.NewLine + value;
+
+                            var stringTable = workbookPart.GetPartsOfType<SharedStringTablePart>().FirstOrDefault();
+                            int value_int = 0;
+
+                            //hay un problema en este parse
+                            if (!int.TryParse(value, out value_int)) value_int = 0;
+
+                            value = stringTable.SharedStringTable.ElementAt(value_int).InnerText;
+                            bool isFound = value.Trim().ToLower().Contains(search.Trim().ToLower());
+
+                            if (isFound)
+                            {
+                                int letterindx = (int)(GetColumnIndex(cell.CellReference) - 1);
+                                index = $"[{letters[letterindx]}{row.RowIndex}]";
+                                logBox.Text = logBox.Text + Environment.NewLine + index+" "+value;
+                            }
+
+                        }
+                    }
+                }
+
+                if (index.Equals("Not Found"))
+                {
+                    logBox.Text = logBox.Text + Environment.NewLine + index;
+                }
+
+            } 
+
+            
+        }
+
+        private static int? GetColumnIndex(string cellReference)
+        {
+            if (string.IsNullOrEmpty(cellReference))
+            {
+                return null;
+            }
+
+            string columnReference = Regex.Replace(cellReference.ToUpper(), @"[\d]", string.Empty);
+
+            int columnNumber = -1;
+            int mulitplier = 1;
+
+            foreach (char c in columnReference.ToCharArray().Reverse())
+            {
+                columnNumber += mulitplier * ((int)c - 64);
+
+                mulitplier = mulitplier * 26;
+            }
+
+            return columnNumber + 1;
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo d = new DirectoryInfo(textBox1.Text); //Assuming Test is your Folder
+
+            FileInfo[] Files = d.GetFiles("*.xlsx"); //Getting Text files
+
+            foreach (FileInfo file in Files)
+            {
+                GetIndexBySearch(searchBox.Text, file.FullName);
+            }
+        }
+    }
+}
